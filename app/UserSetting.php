37caf267c9225e5d750eaab14bf3681f1d16ddd1
Ross Wintle <ross@oikos.org.uk>
2016-11-11 22:18:43 +0000

    Add default time setting for users

--- app/UserSetting.php
+++ app/UserSetting.php
@@ -10,11 +10,13 @@ class UserSetting extends Model
     protected $fillable = [
         'timer_increment',
         'timer_decrement',
+        'default_time',
     ];
 
     protected $attributes = [
         'timer_increment' => 5,
         'timer_decrement' => 5,
+        'default_time' => 3600,
     ];
 
     public function user()
