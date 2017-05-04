2692daeeac03f340fc8747dcda1cccd073c6411e
Ross Wintle <ross@oikos.org.uk>
2017-05-04 13:47:16 +0100

    Add privacy and terms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,16 @@
             <p>There are settings for the increment and decrement amounts, the initial value of the timer
             each day, and the maximum value of the timer.</p>
             <h2>Why bother</h2>
-            <p>As with all my side projects, I'm mostling learning and practicing my coding skills.</p>
+            <p>As with all my side projects, I'm mostly learning and practicing my coding skills.</p>
             <p>This app is written in Laravel and Vue.js, but also explores some interesting CSS techniques.</p>
+            <h2>Privacy and Terms</h2>
+            <p>I need to collect your name and email address to set up an account. This is so I know who you are and
+                how to contact you should I need to, and so that you can log in to Screen Timer.</p>
+            <p>Registering/logging in uses cookies to keep you logged in. I may also use analytics services that use cookies
+                and collect anonymised data.</p>
+            <p>Screen Timer is a side project offered with no guarantee of availability or accuracy. You use it at your
+            own risk. If your kids throw the remote at the telly when time is up then that's your problem! Enjoy!</p>
+            <p>This is a project of <a href="https://rosswintle.uk/">Ross Wintle</a> and <a href="https://oikos.digital/">Oikos Digital</a></p>
         </div>
     </div>
 </div>
